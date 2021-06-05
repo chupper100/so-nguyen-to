00e3c89031d4879d9d84a81d49f93f56e57ef706
Dat0104Alf <>
2021-06-05 03:17:05 +0000

    Works properly.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello', input('What is your name?'))
+a=0
+n=int(input('Nhập số N cần kiểm tra:'))
+
+if n < 2:
+  print(n,'không phải là một số nguyên tố.')
+else:
+  for i in range(n):
+    if n%(i+1)==0:
+      a=a+1
+  if a == 2:
+    print(n,'là một số nguyên tố.')
+  else:
+    print(n,'không phải là một số nguyên tố.')
